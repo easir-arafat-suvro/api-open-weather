9a7d0e08c7a0ef2ac50c3de4082909fdfdc28d14
easir-arafat-suvro <arafat.programmar@gmail.com>
2021-09-28 08:51:01 +0600

    code done

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,10 @@
-// Error massage
-document.getElementById('errorMassage').style.display = 'none';
-
 const loadWeatherData = () => {
     const searchField = document.getElementById('inputField');
     const city = searchField.value;
     searchField.value = '';
+
+    const searchError = document.getElementById('errorMassage');
+    searchError.textContent = '';
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'e372251f13d66913a2adc9bb8722ec0d'}`;
     fetch(url)
@@ -14,14 +14,30 @@ const loadWeatherData = () => {
 }
 
 const errorMassage = () => {
-    document.getElementById('errorMassage').style.display = 'block';
+    const errorMassage = document.getElementById('errorMassage');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <h1 id="errorMassage" class="text-white text-center">Input a valid city name</h1>
+    `;
+    errorMassage.appendChild(div);
 }
 
-const searchInfo = (weatherInfo) => {
-    const cityName = weatherInfo.name;
-    const temparature = weatherInfo.main.temp;
-    const status = weatherInfo.weather[0].main;
-    document.getElementById('location').innerText = cityName;
-    document.getElementById('temp').innerText = temparature;
-    document.getElementById('status').innerText = status;
+const searchInfo = weatherInfo => {
+    const searchWeather = document.getElementById('DisplayWeather');
+    searchWeather.textContent = '';
+
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="">
+    <h1>${weatherInfo.name}</h1>
+    <h3><span>${weatherInfo.main.temp}</span>&deg;C</h3>
+    <h1 class="lead">${weatherInfo.weather[0].main}</h1>    
+`;
+    searchWeather.appendChild(div);
 }
+
+
+
+
+
+
