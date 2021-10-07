0eb99ab2cf2d2730921c651356d48916c8d881da
easir-arafat-suvro <arafat.programmar@gmail.com>
2021-10-07 15:29:26 +0600

    update

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,5 @@
+const API_KEY = `e372251f13d66913a2adc9bb8722ec0d`;
+
 const loadWeatherData = () => {
     const searchField = document.getElementById('inputField');
     const city = searchField.value;
@@ -6,7 +8,7 @@ const loadWeatherData = () => {
     const searchError = document.getElementById('errorMassage');
     searchError.textContent = '';
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${'e372251f13d66913a2adc9bb8722ec0d'}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then(res => res.json())
         .then(data => searchInfo(data))
@@ -28,7 +30,7 @@ const searchInfo = weatherInfo => {
 
     const div = document.createElement('div');
     div.innerHTML = `
-    <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="">
+    <img src="https://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png" alt="">
     <h1>${weatherInfo.name}</h1>
     <h3><span>${weatherInfo.main.temp}</span>&deg;C</h3>
     <h1 class="lead">${weatherInfo.weather[0].main}</h1>    
